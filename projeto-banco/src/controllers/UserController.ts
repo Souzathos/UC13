258b6851373f2abb5259b8a6119f3effc3a798b9
ATHOS FELIPE NASCIMENTO DE SOUZA <04564021010@senacrs.edu.br>
2026-07-31 20:27:08 -0300

    feat: controllers e services

--- projeto-banco/src/controllers/UserController.ts
+++ projeto-banco/src/controllers/UserController.ts
@@ -16,7 +16,7 @@ export class UserController {
     async show(req: Request, res: Response) {
         try {
             const user = await userService.show(Number(req.params.id));
-            return res.json(user);
+            return res.status(200).json(user);
         } catch (error: any) {
             if (error.message === 'User not found') {
                 return res.status(404).json({ message: error.message });
